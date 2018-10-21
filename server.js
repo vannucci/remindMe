f122cce55cd5eaf90abbd1db30bcf2dc6a7a820a
vannucci@gmail.com <vannucci@gmail.com>
2018-10-21 06:43:03 -0400

    Included twilioml service to cut out Logic App

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const accountsid = 'ACca9beca0739df92a37a93586a42f3566';
+const authtoken = 'd57a4e8c105892f4e6fb9642c34bdbd2';
 const port = process.env.PORT || 3000;
 
+
+// mongoose.connect();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 
-app.post('/get', (req, res) => {
+app.post('/sms', (req, res) => {
+  var twiml = new twilio.twiml
   return res.send("Hello, world!");
 });
 
