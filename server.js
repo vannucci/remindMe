21b773bc85dcc5e3f4ad46f6f5dc6629ea6e2550
vannucci@gmail.com <vannucci@gmail.com>
2018-10-22 16:28:51 -0400

    Testing twilio client

--- server.js
+++ server.js
@@ -5,14 +5,27 @@ const accountsid = 'ACca9beca0739df92a37a93586a42f3566';
 const authtoken = 'd57a4e8c105892f4e6fb9642c34bdbd2';
 const port = process.env.PORT || 3000;
 
+const client = require('twilio')(accountsid,authtoken);
+
+client.messages
+  .create({
+    body: "RemindMe! is up and running",
+    from: "+12016251701",
+    to: "+17327786522"
+  })
+  .then(message => console.log(message.sid))
+  .done();
 
 // mongoose.connect();
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 
 app.post('/sms', (req, res) => {
-  var twiml = new twilio.twiml
-  return res.send("Hello, world!");
+  const twiml = new MessagingResponse();
+
+  twiml.message('It\'s not the Volvo you were expecting...<3');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 app.get('/', (req,res) => {
