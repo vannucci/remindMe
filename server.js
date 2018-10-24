742a96701a954c4ca8ddaf6d36d9de7723c8bd3b
vannucci@gmail.com <vannucci@gmail.com>
2018-10-23 20:29:00 -0400

    Split up routing for clarity

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const accountsid = 'ACca9beca0739df92a37a93586a42f3566';
-const authtoken = 'd57a4e8c105892f4e6fb9642c34bdbd2';
-const port = process.env.PORT || 3000;
-
-const client = require('twilio')(accountsid,authtoken);
-
-// client.messages
-//   .create({
-//     body: "RemindMe! is up and running",
-//     from: "+12016251701",
-//     to: "+17327786522"
-//   })
-//   .then(message => console.log(message.sid))
-//   .done();
-
-// mongoose.connect();
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false}));
+require('./routing/routes')(app);
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
+// const accountsid = 'ACca9beca0739df92a37a93586a42f3566';
+// const authtoken = 'd57a4e8c105892f4e6fb9642c34bdbd2';
 
-  twiml.message(`You asked for, ${req.body} at ${req.date_sent}, is that correct?` );
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
-
-app.get('/', (req,res) => {
-  return res.send("Coming soon...");
-});
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
