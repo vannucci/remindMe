80f9edafd469704967b54913e78a486741c809a1
vannucci@gmail.com <vannucci@gmail.com>
2018-10-24 05:38:12 -0400

    I am an idiot

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false}));
 require('./routing/routes')(app);
 
-// const accountsid = 'ACca9beca0739df92a37a93586a42f3566';
-// const authtoken = 'd57a4e8c105892f4e6fb9642c34bdbd2';
 
 
 app.listen(port, () => {
